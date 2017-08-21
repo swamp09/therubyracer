c9133b514d8d79a4b16d0ef78ebe8743b6fab440
swamp09 <shu.numata@esm.co.jp>
2017-08-21 20:07:44 +0900

    Suppress `warning: possibly useless use of == in void context`

--- spec/v8/conversion_spec.rb
+++ spec/v8/conversion_spec.rb
@@ -25,13 +25,13 @@ describe V8::Conversion do
     context "for 32-bit numbers" do
       it "should convert positive integer" do
         cxt['integer_a'] = 123
-        cxt['integer_a'].should == 123
+        cxt['integer_a'].should eq (123)
         cxt['integer_a'].should be_kind_of(Integer)
       end
 
       it "should convert negative integer" do
         cxt['integer_b'] = -123
-        cxt['integer_b'].should == -123
+        cxt['integer_b'].should eq (-123)
         cxt['integer_b'].should be_kind_of(Integer)
       end
     end
